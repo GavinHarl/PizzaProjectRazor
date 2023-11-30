d5cc1c2749249515ade4975eaa310da5ff469adf
DevinJohnson18 <devin_johnson@stu.indianhills.edu>
2023-11-30 12:20:39 -0600

    second commit

---
+++ Pizza.cs
@@ -0,0 +1,46 @@
+﻿namespace BetterPizza
+{
+	public class Pizza
+	{
+		public Topping PizzaTopping { get; set; }
+
+		public Pizza()
+		{
+			PizzaTopping = new Topping();
+		}
+
+		public decimal GetFinalPrice()
+		{
+			return PizzaTopping.ToppingPrice();
+		}
+	}
+
+	public class Topping
+	{
+		public bool Bacon { get; set; }
+
+		public bool MoreBacon { get; set; }
+
+		public bool ExtraBacon { get; set; }
+
+		public decimal ToppingPrice()
+		{
+			decimal runningTotal = 0;
+			if (Bacon)
+			{
+				runningTotal += 30;
+			}
+			if (MoreBacon)
+			{
+				runningTotal += 50;
+			}
+			if (ExtraBacon)
+			{
+				runningTotal += 200;
+			}
+			return runningTotal;
+
+		}
+
+	}
+}
